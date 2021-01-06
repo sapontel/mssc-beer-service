99b54c82527d07fa747846dc976cb3a15eb56670
Saul Osbaldo Aponte Lopez <ingsoal@gmail.com>
2021-01-05 21:42:37 -0500

    Adding beer controller. Class 3

---
+++ src/main/java/sos/siho/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,29 @@
+package sos.siho.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sos.siho.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId")UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> getBeerById(BeerDto beerDto){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId")UUID beerId, @RequestBody BeerDto beerDto){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
